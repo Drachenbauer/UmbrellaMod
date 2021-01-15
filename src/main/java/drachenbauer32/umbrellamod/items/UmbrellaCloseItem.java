e378da4c0af1603cee0eca3d2cd8ce06469107c3
Drachenbauer <47259059+Drachenbauer@users.noreply.github.com>
2021-01-15 17:53:18 +0100

    Update UmbrellaCloseItem.java

--- src/main/java/drachenbauer32/umbrellamod/items/UmbrellaCloseItem.java
+++ src/main/java/drachenbauer32/umbrellamod/items/UmbrellaCloseItem.java
@@ -2,6 +2,7 @@ package drachenbauer32.umbrellamod.items;
 
 import drachenbauer32.umbrellamod.init.UmbrellaItems;
 import drachenbauer32.umbrellamod.util.UmbrellaColors;
+import net.minecraft.block.BlockState;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.EquipmentSlotType;
@@ -11,6 +12,7 @@ import net.minecraft.item.ItemTier;
 import net.minecraft.item.SwordItem;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 public class UmbrellaCloseItem extends SwordItem
@@ -56,6 +58,13 @@ public class UmbrellaCloseItem extends SwordItem
     @Override
     public boolean hitEntity(ItemStack stack, LivingEntity target, LivingEntity attacker)
     {
+        return true;
+    }
+    
+    @Override
+    public boolean onBlockDestroyed(ItemStack stack, World worldIn, BlockState state, BlockPos pos, LivingEntity entityLiving)
+    {
+        // TODO Auto-generated method stub
         return true;
     }
 }
